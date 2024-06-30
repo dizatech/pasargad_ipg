44f3c0144cbb0e681dc7bb2ec97b063df2a1829e
Omid Reza Vasheghani Farahani <omidrezav@gmail.com>
2024-06-30 07:18:25 +0330

    Updated code to conform to new IPG API

--- src/PasargadIpg.php
+++ src/PasargadIpg.php
@@ -2,159 +2,237 @@
 
 namespace Dizatech\PasargadIpg;
 
-class PasargadIpg{
-    
-    protected $merchant_code;
-    protected $terminal_code;
-    protected $private_key;
-    protected $base_url = 'https://pep.shaparak.ir/Api/v1/Payment/';
+use Exception;
+use GuzzleHttp\Client;
+
+class PasargadIpg
+{
+    protected $username;
+    protected $password;
+    protected $terminal_number;
+    protected $client;
     protected $verify_ssl = TRUE;
 
-    public function __construct( $args=[] )
+    const BASE_URI = 'https://pep.shaparak.ir/dorsa1';
+
+    public function __construct($args = [])
     {
-        $this->merchant_code = $args['merchant_code'];
-        $this->terminal_code = $args['terminal_code'];
-        $this->private_key = $args['private_key'];
-    }
-    
-    public function verifySSL( bool $verify=TRUE ){
-		$this->verify_ssl = $verify;
-	}
-    
-    public function getToken( $amount, $invoice_number, $invoice_date, $redirect_address )
-    {
-        $rsa = new Rsa( $this->private_key );
-        $params = [];
-        $params['amount'] = $amount;
-        $params['invoiceNumber'] = $invoice_number;
-        $params['invoiceDate'] = $invoice_date;
-        $params['action'] = '1003'; 	
-        $params['merchantCode'] = $this->merchant_code;
-        $params['terminalCode'] = $this->terminal_code;
-        $params['redirectAddress'] = $redirect_address;
-        $params['timeStamp'] = date('Y/m/d H:i:s');
-        
-        $signature = base64_encode( $rsa->sign( sha1( json_encode($params), TRUE ) ) );
-        $result = $this->send_request( $this->base_url . 'GetToken', json_encode( $params ), $signature );
-        $result = json_decode( $result );
-        
-        $response = new \stdClass();
-        if( $result->IsSuccess ){
-            $response->status = 'success';
-            $response->token = $result->Token;
-        }
-        else{
-            $response->status = 'error';
-            $response->message = $result->Message;
-        }
-        
-        return $response;
-    }
-    
-    public function checkTransaction( $invoice_number, $invoice_date )
-    {
-        $params = [];
-        $params['invoiceNumber'] = $invoice_number;
-        $params['invoiceDate'] = $invoice_date;
-        $params['merchantCode'] = $this->merchant_code;
-        $params['terminalCode'] = $this->terminal_code;
-        $params['timeStamp'] = date("Y/m/d H:i:s");
-        
-        $result = $this->send_request( $this->base_url . 'CheckTransactionResult', json_encode( $params ) );
-        $result = json_decode( $result );
-        
-        $response = new \stdClass();
-        if( $result->IsSuccess ){
-            $response->status = 'success';
-            $response->amount = $result->Amount;
-        }
-        else{
-            $response->status = 'error';
-            $response->message = $result->Message;
-        }
-        
-        return $response;
-    }
-    
-    public function verifyTransaction( $amount, $invoice_number, $invoice_date )
-    {
-        $rsa = new Rsa( $this->private_key );
-        $params = [];
-        $params['amount'] = $amount;
-        $params['invoiceNumber'] = $invoice_number;
-        $params['invoiceDate'] = $invoice_date;
-        $params['merchantCode'] = $this->merchant_code;
-        $params['terminalCode'] = $this->terminal_code;
-        $params['timeStamp'] = date("Y/m/d H:i:s");
-        
-        $signature = base64_encode( $rsa->sign( sha1( json_encode($params), TRUE ) ) );
-        $result = $this->send_request( $this->base_url . 'VerifyPayment', json_encode( $params ), $signature );
-        $result = json_decode( $result );
-        
-        $response = new \stdClass();
-        if( $result->IsSuccess ){
-            $response->status = 'success';
-            $response->shaparak_ref = $result->ShaparakRefNumber;
-        }
-        else{
-            $response->status = 'error';
-            $response->message = $result->Message;
-        }
-        
-        return $response;
-    }
-    
-    public function refundPayment( $invoice_number, $invoice_date )
-    {
-        $rsa = new Rsa( $this->private_key );
-        $params = [];
-        $params['invoiceNumber'] = $invoice_number;
-        $params['invoiceDate'] = $invoice_date;
-        $params['merchantCode'] = $this->merchant_code;
-        $params['terminalCode'] = $this->terminal_code;
-        $params['timeStamp'] = date("Y/m/d H:i:s");
-        
-        $signature = base64_encode( $rsa->sign( sha1( json_encode($params), TRUE ) ) );
-        $result = $this->send_request( $this->base_url . 'RefundPayment', json_encode( $params ), $signature );
-        $result = json_decode( $result );
-        
-        $response = new \stdClass();
-        if( $result->IsSuccess ){
-            $response->status = 'success';
-        }
-        else{
-            $response->status = 'error';
-            $response->message = $result->Message;
-        }
-        
-        return $response;
+        $this->username = $args['username'];
+        $this->password = $args['password'];
+        $this->terminal_number = $args['terminal_number'];
+        $this->client = new Client();
     }
 
-    protected function send_request( $url, $message, $signature="" )
+    public function verifySSL(bool $verify = TRUE)
     {
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HEADER, true);
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $message);
-        if( !$this->verify_ssl ){
-			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-		}
-
-        $headers[] = 'Content-Type: application/json';
-        $headers[] = 'Accept: application/json';
-        if($signature != ''){
-            $headers[] = 'Sign: '.$signature;
-        }
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-        $response = curl_exec($ch);
-        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
-        $body = substr($response, $header_size);
-        curl_close($ch);
-        return $body;
+        $this->verify_ssl = $verify;
     }
-    
+
+    public function purchase($amount, $invoice_number, $invoice_date, $redirect_address)
+    {
+        $result = new \stdClass();
+        $result->status = 'error';
+        $token = $this->getToken();
+
+        if ($token) {
+            try {
+                $response = $this->client->post(
+                    self::BASE_URI . '/api/payment/purchase',
+                    [
+                        'headers'   => [
+                            'Authorization'     => "Bearer {$token}",
+                        ],
+                        'body'      => json_encode([
+                            'amount'            => strval($amount),
+                            'invoice'           => strval($invoice_number),
+                            'invoiceDate'       => $invoice_date,
+                            'callbackApi'       => $redirect_address,
+                            'serviceCode'       => 8,
+                            'serviceType'       => 'PURCHASE',
+                            'terminalNumber'    => $this->terminal_number,
+                        ]),
+                    ]
+                );
+
+                if ($response->getStatusCode() == 200) {
+                    $response = json_decode($response->getBody()->getContents());
+                    if ($response && isset($response->resultCode) && $response->resultCode == 0) {
+                        $result->status = 'success';
+                        $result->token = $response->data->urlId;
+                        $result->payment_url = $response->data->url;
+                    }
+                }
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+            }
+        }
+
+        return $result;
+    }
+
+    public function inquiry($invoice_number)
+    {
+        $result = new \stdClass();
+        $result->status = 'error';
+        $token = $this->getToken();
+
+        if ($token) {
+            try {
+                $response = $this->client->post(
+                    self::BASE_URI . '/api/payment/payment-inquiry',
+                    [
+                        'headers'   => [
+                            'Authorization'     => "Bearer {$token}",
+                        ],
+                        'body'      => json_encode([
+                            'invoiceId'         => strval($invoice_number),
+                        ]),
+                    ]
+                );
+
+                if ($response->getStatusCode() == 200) {
+                    $response = json_decode($response->getBody()->getContents());
+                    if ($response && isset($response->resultCode) && $response->resultCode == 0) {
+                        switch ($response->data->status) {
+                            case 2:
+                                $result->status = 'success';
+                                $result->payment_status = 'success';
+                                break;
+                            case 13029:
+                                $result->status = 'success';
+                                $result->payment_status = 'confirmed';
+                                break;
+                            case 13033:
+                                $result->status = 'success';
+                                $result->payment_status = 'refunded';
+                                break;
+                        }
+                        $result->transaction_id = $response->data->transactionId;
+                        $result->reference_number = $response->data->referenceNumber;
+                        $result->amount = $response->data->amount;
+                        $result->pan = $response->data->cardNumber;
+                        $result->url_id = $response->data->url;
+                    } elseif (isset($response->resultCode)) {
+                        error_log("Payment inquiry for invoice {$invoice_number} failed with {$response->resultCode} error.");
+                    } else {
+                        error_log("Payment inquiry for invoice {$invoice_number} failed.");
+                    }
+                }
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+            }
+        }
+
+        return $result;
+    }
+
+    public function verify($invoice_number, $url_id)
+    {
+        $result = new \stdClass();
+        $result->status = 'error';
+        $token = $this->getToken();
+
+        if ($token) {
+            try {
+                $response = $this->client->post(
+                    self::BASE_URI . '/api/payment/verify-payment',
+                    [
+                        'headers'   => [
+                            'Authorization'     => "Bearer {$token}",
+                        ],
+                        'body'      => json_encode([
+                            'invoice'           => strval($invoice_number),
+                            'urlId'             => $url_id,
+                        ]),
+                    ]
+                );
+
+                if ($response->getStatusCode() == 200) {
+                    $response = json_decode($response->getBody()->getContents());
+                    if ($response && isset($response->resultCode) && $response->resultCode == 0) {
+                        $result->status = 'success';
+                        $result->reference_number = $response->data->referenceNumber;
+                        $result->amount = $response->data->amount;
+                        $result->pan = $response->data->maskedCardNumber;
+                    } else {
+                        error_log("Verifying transaction for invoice {$invoice_number} failed.");
+                    }
+                }
+            } catch (Exception $e) {
+                error_log($e->getMessage());
+            }
+        }
+
+        return $result;
+    }
+
+    public function refund($invoice_number, $url_id)
+    {
+        $result = new \stdClass();
+        $result->status = 'error';
+        $token = $this->getToken();
+
+        if ($token) {
+            $inquiry = $this->inquiry(invoice_number: $invoice_number);
+            if ($inquiry->status == 'success') {
+                if ($inquiry->payment_status == 'success') {
+                    try {
+                        $response = $this->client->post(
+                            self::BASE_URI . '/api/payment/reverse-transactions',
+                            [
+                                'headers'   => [
+                                    'Authorization'     => "Bearer {$token}",
+                                ],
+                                'body'      => json_encode([
+                                    'invoice'           => strval($invoice_number),
+                                    'urlId'             => $url_id,
+                                ]),
+                            ]
+                        );
+        
+                        if ($response->getStatusCode() == 200) {
+                            $response = json_decode($response->getBody()->getContents());
+                            if ($response && isset($response->resultCode) && $response->resultCode == 0) {
+                                $result->status = 'success';
+                            } else {
+                                error_log("Reversing transaction for invoice {$invoice_number} failed.");
+                            }
+                        }
+                    } catch (Exception $e) {
+                        error_log($e->getMessage());
+                    }
+                } elseif ($inquiry->payment_status == 'verified') {
+                    $result->error_message = 'تراکنش مورد نظر تایید نهایی شده و قابل برگشت نیست.';
+                } elseif ($inquiry->payment_status == 'refunded') {
+                    $result->error_message = 'تراکنش مورد نظر قبلا برگشت داده شده است.';
+                }
+            } else {
+                $result->error_message = 'تراکنش مورد نظر ناموفق است.';
+            }
+        }
+
+        return $result;
+    }
+
+    public function getToken()
+    {
+        $response = $this->client->post(
+            self::BASE_URI . '/token/getToken',
+            [
+                'body'      => json_encode([
+                    'username'  => $this->username,
+                    'password'  => $this->password,
+                ]),
+            ]
+        );
+        if ($response->getStatusCode() == 200) {
+            $response = json_decode($response->getBody()->getContents());
+            if ($response && isset($response->resultCode) && $response->resultCode == 0) {
+                return $response->token;
+            }
+        }
+
+        error_log('Failed to connect to Pasargad IPG API to acquire token');
+        return false;
+    }
 }
